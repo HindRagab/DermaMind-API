949aee9ae45100ca45079a8daad37de21c086691
هند رجب <hindr380@gmail.com>
2026-05-24 00:31:17 +0300

    fix

--- DermaMindProject1/Controllers/AuthController.cs
+++ DermaMindProject1/Controllers/AuthController.cs
@@ -9,6 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace DermaApp.API.Controllers
 {

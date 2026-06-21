7bfd177809b0fff410862762e1c13d64af076584
هند رجب <hindr380@gmail.com>
2026-06-22 02:19:25 +0300

    fix

--- DermaMindProject1/Controllers/ProfileController.cs
+++ DermaMindProject1/Controllers/ProfileController.cs
@@ -137,8 +137,9 @@ namespace DermaApp.API.Controllers
         }
 
         public class ChangePasswordDto
-    {
-        public string CurrentPassword { get; set; } = string.Empty;
-        public string NewPassword { get; set; } = string.Empty;
+        {
+            public string CurrentPassword { get; set; } = string.Empty;
+            public string NewPassword { get; set; } = string.Empty;
+        }
     }
 }
